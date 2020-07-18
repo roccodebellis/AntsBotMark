64a5a5ef059ff300c495b8a428bfa169140e0869
Rocco Debellis <rocdebellis@gmail.com>
2020-07-18 18:43:45 +0200

    added new param

--- src/defaultpackage/Configuration.java
+++ src/defaultpackage/Configuration.java
@@ -3,10 +3,17 @@ package defaultpackage;
 public class Configuration {
 	
 	private static final int CombatModuleMinMaxMaxDepth = 6;
+	private static final int MilSecUsedForEachAntsInCS = 10;
 	
 	public static int getCombatModuleMinMaxMaxDepth(){
 		return CombatModuleMinMaxMaxDepth;
 	}
+	
+	/*tempo presunto utilizzato da combattimento per simulare la battaglia con una formica*/
+	public static int getMilSecUsedForEachAntsInCS(){
+		return MilSecUsedForEachAntsInCS;
+	}
+	
 	
 	
 }
